9279af860c3cbe1a2b2c15dbffa4a2c3e1be9bd5
Atikkhan1 <khanarbajpathan057@gmail.com>
2024-07-08 18:27:57 +0530

    new login and image category

--- components/Categorybar.jsx
+++ components/Categorybar.jsx
@@ -1,26 +1,32 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Categorybar = () => {
 
     let optionsData = [
-        {name:"Tshirts",link:"/category/tshirts"},
-        {name:"Toys",link:"/category/toys"},
-        {name:"Shoes",link:"/category/shoes"},
-        {name:"Watch",link:"/category/watch"},
-        {name:"Hoodies",link:"/category/hoodies"},
-        {name:"Shirts",link:"/category/shirts"},
-        {name:"Jeans",link:"/category/jeans"},
-        {name:"Shorts",link:"/category/shorts"},
-        {name:"earbuds",link:"/category/earbuds"},
+        {name:"Tshirts",link:"/category/tshirts",img:"/categoryImage/tshirts.jpg" },
+        {name:"Toys ",link:"/category/toys",img:"/categoryImage/toys.jpg" },
+        {name:"Shoes",link:"/category/shoes",img:"/categoryImage/shoes.jpg" },
+        {name:"Watch",link:"/category/watch",img:"/categoryImage/watch.jpg" },
+        {name:"Hoodies",link:"/category/hoodies",img:"/categoryImage/hoodies.jpg" },
+        {name:"Shirts",link:"/category/shirts",img:"/categoryImage/shirts.jpg" },
+        {name:"Earbuds",link:"/category/earbuds",img:"/categoryImage/earbuds.jpg" },
       ]
     
 
   return (
-    <div className={`flex w-full h-10  max-md:overflow-x-auto bg-gray-100 gap-3 p-1 shadow-inner shadow-gray-400`}>
+    <div className={`flex w-full max-md:overflow-x-auto bg-gray-100 gap-3 p-1 shadow-inner shadow-gray-400`}>
     {optionsData.map((d)=>{
     return (
-    <Link key={d.name} href={d.link} className='flex h-full w-auto px-2 pt-0.5 text-xs rounded-lg md:text-xl   font-extrabold font-mono justify-center text-black items-center'>{d.name}</Link>
+    <Link key={d.name} href={d.link} className='inline-block w-auto items-center px-2 pt-0.5 text-xs rounded-lg md:text-xl font-extrabold font-mono text-black '>
+      <div className='flex w-full h-8 overflow-hidden justify-center items-end'>
+      <Image src={d.img}
+    alt="/" width={'34'} height={'2'}  className=''/>
+      </div>
+    <p className='flex  '>{d.name}</p>
+    
+    </Link>
 )})}
 
   </div>
--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Searchbar = (p) => {
   return (
-    <div className={'flex h-10 md:m-auto md:w-1/2 max-md:w-full border-4 border-current rounded-full bg-pink-600 text-pink-600 ' + p.className}>
-      <input className='w-full text-black rounded-l-full indent-4 focus:outline-none ' type="text" name="" id="" />
+    <div className={'flex h-10 md:m-auto md:w-1/2 max-md:w-full border-4 border-current rounded-full bg-green-400 text-green-400 ' + p.className}>
+      <input className='w-full text-green-400 rounded-l-full indent-4 focus:outline-none ' type="text" name="search" id="search" placeholder='search products ...'/>
       <Link href={'/'} className='flex my-auto mx-2 text-white' >
       <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" ><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
       </Link>
